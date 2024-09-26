d08f70fa59a5666e5997608ccfc563d81a502da4
Peng <peng37@tutanota.com>
2024-09-26 11:56:12 +0200

    fix floating button

--- src/components/FloatAction.jsx
+++ src/components/FloatAction.jsx
@@ -4,21 +4,29 @@ import React from 'react';
 
 export function FloatAction({ icon, onClick, style }) {
   return (
-    <Button
-      fill="solid"
-      type="primary"
-      icon={icon || <RiAddLine />}
-      onClick={onClick}
-      shape="round"
+    <div
       style={{
-        ...(style || {}),
-        position: 'absolute',
+        position: 'sticky',
         top: 'calc(100% - 7rem)',
         left: 'calc(100% - 5rem)',
         zIndex: 3,
         width: 48,
-        height: 48,
+        height: 0,
+        overflow: 'visible',
       }}
-    />
+    >
+      <Button
+        fill="solid"
+        type="primary"
+        icon={icon || <RiAddLine />}
+        onClick={onClick}
+        shape="round"
+        style={{
+          ...(style || {}),
+          width: 48,
+          height: 48,
+        }}
+      />
+    </div>
   );
 }
